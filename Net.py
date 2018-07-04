23356559b8c5676af60afe33deacf4086ba53d34
UlysseREGLADE <ulysser@DESKTOP-TDCUMR5.localdomain>
2018-07-04 15:06:10 +0200

    toujours une version buggue

--- Net.py
+++ Net.py
@@ -1,13 +1,22 @@
 import tensorflow as tf
+import numpy as np
+import warnings
 
 class Net(object):
 
     #Constructeur
     def __init__(self, name, is_training=True):
+        #Nom du variable_scope du graph
         self.name = name
+        #Ces variables permettent de gerer la batch_norm si besoin
         self.is_training = is_training
         self.is_batch_norm = False
+        #Nombre de variables dans le reseau
         self.n_parameters = 0
+        #Coefficient pour l'initialisation de xavier
+        self.std_coef = 1
+        #Initialisation par defaut
+        self.default_init = "uniform"
 
     def output(self, i_input):
         self.scopes_and_variables = []
@@ -15,6 +24,14 @@ class Net(object):
         with tf.varible_scope(self.name, reuse = tf.AUTO_REUSE):
             l_output = self.net(i_input)
         return l_output
+
+    def net():
+        #C'est dans cette fonction que doit etre defini le graph de calcule
+        raise NotImplementedError
+
+    def def_net():
+        #C'est dans cette fonction que doit etre defini le graph de calcule
+        warnings.warn('def_net not implemented, but called')
 
     def get_name(name):
         index=''
@@ -39,24 +56,30 @@ class Net(object):
                                                       is_training=is_training)
             return normalized
 
-    def conv(self, x, out_channels, kernel=3, name="conv"):
+    def conv(self, x, out_channels, kernel=3, init=None, name="conv"):
         name = self.get_name(name)
         with tf.variable_scope(name):
             #On calcule la variance relative a l'initialisation des poids
             shape = x.get_shape().as_list()
             in_channels = shape[3]
-            stddev = (2/(shape[1]*shape[2]*(in_channels+out_channels)))**0.5
-            init = tf.truncated_normal(shape, stddev=stddev)
+            if(init is None):
+                init = self.default_init
+            if(init=="normal"):
+                stddev = (2/(shape[1]*shape[2]*(in_channels+out_channels)))**0.5
+            elif(init=="uniform"):
+                stddev = (6/(shape[1]*shape[2]*(in_channels+out_channels)))**0.5
+            stddev *= self.std_coef
             #Puis on initialise les variables
-            l_w = self.var([kernel, kernel, in_channels, out_channels],
-                           l_stddev,
+            w = self.var([kernel, kernel, in_channels, out_channels],
+                           stddev,
+                           init,
                            name="weigth")
-            l_b = self.var([out_channels], l_stddev, name="bias")
+            b = self.var([out_channels], 0, name="bias")
             #Enfin, on retourne ce qu'il faut
-            l_conv = tf.nn.conv2d(x, l_w, strides=[1, 1, 1, 1], padding="SAME")
-            return (l_conv + l_b)
+            conv = tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding="SAME")
+            return (conv + b)
 
-    def fcon(self, x, out_size, stddev=None, name="fcon"):
+    def fcon(self, x, out_size, init=None, name="fcon"):
         name = self.get_name(name)
         with tf.variable_scope(name):
             #On commence par gerer la taille du tenseur d'entree
@@ -67,28 +90,37 @@ class Net(object):
                     in_size *= shape[i]
                 x = tf.reshape(x, [-1, in_size])
             #On calcule la variance relative a l'initialisation des poids
-            if(stddev is None):
-                stddev = (2/(in_size+out_size))**0.5
+            if(init is None):
+                init = self.default_init
+            if(init=="normal"):
+                stddev = (2/(shape[1]*shape[2]*(in_channels+out_channels)))**0.5
+            elif(init=="uniform"):
+                stddev = (6/(shape[1]*shape[2]*(in_channels+out_channels)))**0.5
+            stddev *= self.std_coef
             #Puis on initialise les variables
-            l_w = self.var([in_size, out_size], stddev, name="weigth")
-            l_b = self.var([out_size], name="bias")
+            w = self.var([in_size, out_size], stddev, name="weigth")
+            b = self.var([out_size], 0, name="bias")
             #Enfin, on retourne ce qu'il faut
-            return tf.matmul(x, l_w) + l_b
+            return tf.matmul(x, w) + b
 
-    def var(self, shape, stddev=0, name="var"):
+    def var(self, shape, stddev=0, init="const", name="var"):
         #On manage le nom de la variable pour ne pas avoir de pb
         name = self.get_name(name)
+        #On calcule l'initialiseur
+        if(init=="normal"):
+            init=tf.truncated_normal(shape, stddev=stddev)
+        elif(init=="uniform"):
+            init=tf.random_uniform(shape, stddev=stddev)
+        elif(init=="const"):
+            init = stddev*np.ones((shape))
         #Puis on la creer
-        var = tf.get_variable(name=name,
-                              initializer=tf.truncated_normal(shape,
-                                                              stddev=stddev))
-        #calcule du nombre de variables
-        shape = var.shape
+        var = tf.get_variable(name=name, initializer=init)
+        #actualisation du nombre de variables dans le graph
         n_param = 1
         for axis in shape:
             n_param *= int(axis)
         self.n_parameters += n_param
-        return l_var
+        return var
 
     def trainer(self, loss, trainer=tf.train.AdamOptimizer()):
         #On recupere toutes les variables du graph a entrainer
