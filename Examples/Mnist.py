f8a8995125b40ad9d0328a514f4f2238b31d5e42
Ulysse Reglade <ulysser@ragshiel.uio.no>
2018-08-14 17:16:40 +0200

    ajout de l'evaluateur

--- Examples/Mnist.py
+++ Examples/Mnist.py
@@ -35,30 +35,32 @@ class MnistModel(Model):
         clas = Classifier('clas', self.is_training)
         clas.set_net()
 
-        self.clas_input = tf.placeholder(tf.float32,
-                                         shape=[None, 28, 28, 1],
-                                         name="pi_input")
+        self.input = tf.placeholder(tf.float32,
+                                    shape=[None, 28, 28, 1],
+                                    name="pi_input")
 
-        self.clas_output = clas.output(self.clas_input)
+        self.output = clas.output(self.input)
 
-        self.clas_ref_output = tf.placeholder(tf.float32,
-                                              shape=[None, 10],
-                                              name="pi_ref_output")
+        self.ref_output = tf.placeholder(tf.float32,
+                                         shape=[None, 10],
+                                         name="pi_ref_output")
 
-        self.clas_loss = htf.celoss(self.clas_output, self.clas_ref_output)
-        self.clas_trainer = clas.trainer(self.clas_loss,
-                                         tf.train.AdamOptimizer(0.0002, 0.5))
+        self.loss = htf.celoss(self.output, self.ref_output)
+        self.trainer = clas.trainer(self.loss,
+                                    tf.train.AdamOptimizer(0.0002, 0.5))
+
+        self.input_list = [self.input]
 
     def train_op(self, sess, batch, count):
 
         batch_x, batch_y_ref = batch.train(100)
-        _, batch_y = sess.run((self.clas_trainer, self.clas_output),
-                               feed_dict={self.clas_input:batch_x,
-                                          self.clas_ref_output:batch_y_ref})
+        _, batch_y = sess.run((self.trainer, self.output),
+                               feed_dict={self.input:batch_x,
+                                          self.ref_output:batch_y_ref})
 
         test_x, test_y_ref = batch.test(1000)
-        test_y = self.clas_output.eval(session=sess,
-                                       feed_dict={self.clas_input:test_x})
+        test_y = self.output.eval(session=sess,
+                                  feed_dict={self.input:test_x})
 
         return {"acc_test" : htf.compute_acc(test_y, test_y_ref),
                 "acc_train" : htf.compute_acc(batch_y, batch_y_ref)}
@@ -66,4 +68,7 @@ class MnistModel(Model):
 
 
 model = MnistModel(name="mnist_model")
-model.train(batch=MnistBatch(), epochs=10, display=10, save=10)
+#model.train(batch=MnistBatch(), epochs=10, display=10, save=10)
+
+with model.default_evaluator() as eval:
+    eval.compute( np.zeros((1,28,28,1)) )
