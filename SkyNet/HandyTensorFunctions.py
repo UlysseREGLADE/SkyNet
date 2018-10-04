e234ce07ef95796f1decff1b1b1f6ea5b3ed3fed
UlysseREGLADE <rider2fois@yahoo.fr>
2018-10-04 15:07:12 +0200

    implemented gan achitecture

--- SkyNet/HandyTensorFunctions.py
+++ SkyNet/HandyTensorFunctions.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import numpy as np
 
+#Utile constant
+
+eps=1e-10
+
 #Activation functions
 
 def lrelu(x, slope=0.2, name="lrelu"):
@@ -15,7 +19,7 @@ def outrelu(x, name="outrelu"):
 
 def celoss(y, y_ref, name="celoss"):
     with tf.variable_scope(name):
-        y = tf.clip_by_value(y, 1e-10, 1)
+        y = tf.clip_by_value(y, eps, 1)
         return tf.reduce_mean(-y_ref*tf.log(y))
 
 def l1loss(y, y_ref, name="l1loss"):
@@ -28,7 +32,7 @@ def l2loss(y, y_ref, name="l2loss"):
 
 def ce2Dloss(y, y_ref, name="ce2Dloss"):
     with tf.variable_scope(name):
-        y = tf.clip_by_value(y, 1e-10, 1-(1e-10))
+        y = tf.clip_by_value(y, eps, 1-(eps))
         return tf.reduce_mean(-y_ref*tf.log(y) - (1-y_ref)*tf.log(1-y))
 
 #Pooling functions
