c93f96780e0200664c69568c3da465c84ab4269c
UlysseREGLADE <rider2fois@yahoo.fr>
2018-10-05 15:19:32 +0200

    unstable version

--- Examples/GANMnist.py
+++ Examples/GANMnist.py
@@ -28,7 +28,7 @@ class Discriminer(Net):
         with tf.variable_scope("fcon3_layer"):
             l_l = self.fcon(l_l, 11)
             l_l = tf.nn.softmax(l_l)
-            return l_l[:, :10]
+            return l_l[:, :10], l_l[:, 10]
 
 class Generator(Net):
 
@@ -64,7 +64,10 @@ class GANMnistModel(Model):
         self.gen_output = gen.output(self.gen_input)
         self.disc_false_input = self.gen_output
 
-        self.disc_false_output = disc.output(self.disc_false_input)
+        self.disc_false_output, disc_false_output_last = disc.output(self.disc_false_input)
+
+        gradient = tf.gradients(disc_false_output_last,
+                                [self.disc_false_input])[0]
 
         self.disc_true_input = tf.placeholder(tf.float32,
                                               shape=[None, 28, 28, 1],
@@ -72,16 +75,14 @@ class GANMnistModel(Model):
         self.disc_true_output_ref = tf.placeholder(tf.float32,
                                                    shape=[None, 10],
                                                    name="disc_true_output_ref")
-        self.disc_true_output = disc.output(self.disc_true_input)
+        self.disc_true_output, _ = disc.output(self.disc_true_input)
 
-        self.gen_loss = tf.reduce_mean(-tf.log(tf.clip_by_value(1-tf.reduce_sum(self.disc_false_output,
-                                                                  axis=1),
+        self.gen_loss = tf.reduce_mean(-tf.log(tf.clip_by_value(disc_false_output_last,
                                                          htf.eps, 1)))
-        self.disc_false_loss = tf.reduce_mean(-tf.log(tf.clip_by_value(tf.reduce_sum(self.disc_false_output,
-                                                                       axis=1),
-                                                                htf.eps, 1)))
+        self.disc_false_loss = tf.reduce_mean(-tf.log(tf.clip_by_value(disc_false_output_last,
+                                                                htf.eps, 1))) + tf.reduce_sum(tf.abs(gradient))
         self.disc_true_loss = htf.celoss(self.disc_true_output,
-                                         self.disc_true_output_ref)
+                                         self.disc_true_output_ref) + tf.reduce_sum(tf.abs(gradient))
 
         self.gen_trainer = gen.trainer(self.gen_loss,
                                        tf.train.AdamOptimizer())
@@ -123,10 +124,13 @@ model = GANMnistModel(name="gan_mnist_model")
 #model.train(batch=MnistBatch(), epochs=10, display=10, save=10)
 
 with model.default_evaluator() as eval:
-    gan_input = np.random.normal(0, 1, (1, 10))
+    gan_input = np.random.normal(0, 1, (2, 10))
 
     gan_output = eval.compute(gan_input)
 
+    plt.figure()
+    plt.imshow(gan_output[1, :, :, 0])
+    plt.show(False)
     plt.figure()
     plt.imshow(gan_output[0, :, :, 0])
     plt.show()
