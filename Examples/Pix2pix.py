89d2990aa8f9ffbdd7358d432eeab3dd34b4f384
UlysseREGLADE <rider2fois@yahoo.fr>
2019-07-05 22:02:43 +0200

    debugged matplotlib

--- Examples/Pix2pix.py
+++ Examples/Pix2pix.py
@@ -154,8 +154,8 @@ class Pix2pixModel(Model):
         clear_output(wait=True)
         plt.figure(figsize=(15,15))
         f, (ax1, ax2, ax3) = plt.subplots(1, 3)
-        ax1.imshow(gen_input)
-        ax2.imshow(disc_true_input)
+        ax1.imshow(gen_input[0,:,:,0])
+        ax2.imshow(disc_true_input[0,:,:,0])
         ax3.imshow(gen_output[0,:,:,0])
         plt.show()
 
